0483eceb43cb6d51ebe74f548507358d7d4e8c31
Kendra Kleber <league@Mac-mini-2.local>
2019-05-18 11:25:59 -0700

    Just need number 5

--- src/Houses.java
+++ src/Houses.java
@@ -1,45 +1,80 @@
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
-	static Robot turtle = new Robot();
-	Houses pointy = new Houses();
-	Houses flat = new Houses();
-	Houses grass = new Houses();
+	static Robot turtle;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 10; i++)
-			;
-
-	}
-
-	public void pointyRoof() {
-		turtle.turn(45);
-		turtle.move(90);
-		turtle.turn(45);
-		turtle.move(90);
-	}
-
-	public void drawHouse() {
+		turtle = new Robot();
 		turtle.moveTo(50, 550);
-		turtle.move(100);
-		pointy.pointyRoof();
+		turtle.penDown();
+		turtle.setPenWidth(2);
+
+		for (int i = 0; i < 10; i++) {
+			turtle.setSpeed(100);
+			turtle.setRandomPenColor();
+			String height = JOptionPane
+					.showInputDialog("Which house would you like to draw between small,medium and large?");
+			if (height.equalsIgnoreCase("small")) {
+				drawsmallHouse();
+			} else if (height.equalsIgnoreCase("medium")) {
+				drawmediumHouse();
+			} else if (height.equalsIgnoreCase("large")) {
+				drawBuildings();
+			}
+		}
+
 	}
 
-	public void flatRoof() {
-		turtle.move(90);
-	}
-
-	public void drawBuildings() {
-		turtle.move(300);
+	public static void pointyRoof() {
+		turtle.turn(45);
+		turtle.move(20);
 		turtle.turn(90);
-		flat.flatRoof();
+		turtle.move(20);
+		turtle.turn(45);
+
 	}
 
-	public void drawGrass() {
+	public static void drawmediumHouse() {
+		turtle.setRandomPenColor();
+		turtle.move(120);
+		pointyRoof();
+		turtle.move(120);
+		turtle.turn(-90);
+		drawGrass();
+	}
+
+	public static void drawsmallHouse() {
+		turtle.setRandomPenColor();
+		turtle.move(60);
+		pointyRoof();
+		turtle.move(60);
+		turtle.turn(-90);
+		drawGrass();
+	}
+
+	public static void flatRoof() {
+		turtle.move(20);
+	}
+
+	public static void drawBuildings() {
+		turtle.setRandomPenColor();
+		turtle.move(250);
+		turtle.turn(90);
+		flatRoof();
+		turtle.turn(90);
+		turtle.move(250);
+		turtle.turn(-90);
+		drawGrass();
+	}
+
+	public static void drawGrass() {
 		turtle.setPenColor(23, 124, 65);
 		turtle.move(20);
 		turtle.setPenWidth(5);
+		turtle.turn(-90);
 
 	}
 
