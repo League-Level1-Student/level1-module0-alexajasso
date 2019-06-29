5f35d63390e9823f9bcbab6e66cace5da17e2783
alexajasso <league@WTS-MM-7.local>
2019-06-29 11:31:04 -0700

    finished houses

--- src/Houses.java
+++ src/Houses.java
@@ -5,22 +5,22 @@ import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 	static Robot turtle;
 
-	public static void main(String[] args) 
+	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
 		turtle = new Robot();
 		turtle.moveTo(50, 550);
 		turtle.penDown();
 		turtle.setPenWidth(2);
-
+		turtle.setWindowColor(0,0,0);
 		for (int i = 0; i < 10; i++) {
 			turtle.setSpeed(100);
 			turtle.setRandomPenColor();
 			String height = JOptionPane
 					.showInputDialog("Which house would you like to draw between small,medium and large?");
 			if (height.equalsIgnoreCase("small")) {
-				drawsmallHouse();
+				drawSmallHouse();
 			} else if (height.equalsIgnoreCase("medium")) {
-				drawmediumHouse();
+				drawMediumHouse();
 			} else if (height.equalsIgnoreCase("large")) {
 				drawBuildings();
 			}
@@ -37,7 +37,7 @@ public class Houses {
 
 	}
 
-	public static void drawmediumHouse() {
+	public static void drawMediumHouse() {
 		turtle.setRandomPenColor();
 		turtle.move(120);
 		pointyRoof();
@@ -46,7 +46,7 @@ public class Houses {
 		drawGrass();
 	}
 
-	public static void drawsmallHouse() {
+	public static void drawSmallHouse() {
 		turtle.setRandomPenColor();
 		turtle.move(60);
 		pointyRoof();
