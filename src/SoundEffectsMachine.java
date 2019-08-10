355a0231910fce4a1e76ce3ef548d99a7d82456b
tactreconx <league@WTS-MM-11.local>
2019-08-10 11:24:05 -0700

    Finished sound effects machine

--- src/SoundEffectsMachine.java
+++ src/SoundEffectsMachine.java
@@ -8,6 +8,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class SoundEffectsMachine implements ActionListener {
+	JButton button1;
+	JButton button2;
+	JButton button3;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -19,17 +22,34 @@ public class SoundEffectsMachine implements ActionListener {
 		frame.setVisible(true);
 		JPanel panel = new JPanel();
 		frame.add(panel);
-		JButton button = new JButton("hi");
-		panel.add(button);
+		button1 = new JButton("1");
+		button2 = new JButton("2");
+		button3 = new JButton("3");
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		frame.add(panel);
 		frame.pack();
-		button.addActionListener(this);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		playSound ("wav-sounds.png");
+		// playSound ("service-bell_daniel_simion.wav");
+		if (e.getSource() == button1) {
+			playSound("service-bell_daniel_simion.wav");
+		} else if (e.getSource() == button2) {
+			playSound("water-drops-daniel_simon.wav");
+		} else if (e.getSource() == button3) {
+			playSound("old-fashioned-school-bell-daniel_simon.wav");
+		}
 
+		{
+
+		}
 	}
 
 	private void playSound(String fileName) {
