79061f2bf0b87623c3fbf51944465bd62587828b
root <alexajasso>
2020-05-23 18:28:10 +0000

    Working on Intro to Constructor.

--- src/IntroToConstructor/Platypus.java
+++ src/IntroToConstructor/Platypus.java
@@ -2,7 +2,7 @@ package IntroToConstructor;
 
 public class Platypus {
 	      private String name;
-
+	  //  new Platypus("bob");
 	       void sayHi(){
 	             System.out.println("The platypus " + name + " is smarter than your average platypus.");
 	       }
---
+++ src/IntroToConstructor/PlatypusRunner.java
@@ -0,0 +1,9 @@
+package IntroToConstructor;
+
+public class PlatypusRunner {
+	public static void main(String[] args) {
+		Platypus a = new Platypus();
+		a.sayHi();
+	}
+
+}
